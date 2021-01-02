3b0f18457309960bdad48b7faa79c1707a913080
mahi <mohiddinoshim@yahoo.com>
2021-01-02 10:52:08 +0600

    First document created

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,11 +1,10 @@
 class DocsController < ApplicationController
-
+	before_action :find_doc, only: [:show, :edit, :update, :destroy] 
 	def index
 		
 	end
 
 	def show
-		
 	end
 
 	def new
@@ -37,7 +36,7 @@ class DocsController < ApplicationController
 	private
 
 	def find_doc
-		
+		@doc = Doc.find(params[:id])
 	end
 
 	def doc_params
